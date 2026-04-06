b794b92f597ff0c3a5d7cdb41ab21cc037ae06fa
Aft01 <aftabn216@gmail.com>
2026-04-07 00:47:46 +0530

    updated gemini embeddingvmodel

--- backend/main.py
+++ backend/main.py
@@ -56,7 +56,7 @@ def embed(texts):
 
         for text in texts:
             res = genai.embed_content(
-                model="models/embedding-001",
+                model="models/text-embedding-004",
                 content=text
             )
             embeddings.append(res["embedding"])
